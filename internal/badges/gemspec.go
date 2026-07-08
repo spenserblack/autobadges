db9315afd8969e2961bb3cf06a225b01a6144650
Spenser Black <me@spenser.black>
2026-07-08 18:24:12 +0000

    Simplify Ruby Gem name detection
    This now uses the filename of the `.gemspec` file to determine the gem
    name.

--- internal/badges/gemspec.go
+++ internal/badges/gemspec.go
@@ -2,11 +2,8 @@ package badges
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
-	"regexp"
-
-	"github.com/spenserblack/autobadges/internal/stringutil"
+	"strings"
 )
 
 // GemSpec will generate a badge.fury.io badge for each Ruby Gem. Will return an empty or nil slice
@@ -19,56 +16,17 @@ func GemSpec(root string) []string {
 	}
 	badges := make([]string, 0, len(matches))
 
-	for _, gemspec := range matches {
-		raw, err := os.ReadFile(gemspec)
-		if err != nil {
-			continue
-		}
-		name := gemSpecNameBytes(raw)
-		if name == "" {
-			continue
-		}
-
-		badge := fmt.Sprintf("[![Gem Version](https://badge.fury.io/rb/%[1]s.svg)](https://badge.fury.io/rb/%[1]s)", name)
+	for _, path := range matches {
+		badge := gemSpecFromPath(path)
 		badges = append(badges, badge)
 	}
 
 	return badges
 }
 
-// gemSpecNameBytes reads bytes representing a .gemspec file and attempts to get the spec's name.
-func gemSpecNameBytes(b []byte) string {
-	v := gemSpecVarBytes(b)
-	if v == "" {
-		return ""
-	}
-	rawPattern := fmt.Sprintf(`\s+%s\.name\s*=\s*(.+)\n`, regexp.QuoteMeta(v))
-	pattern, err := regexp.Compile(rawPattern)
-	if err != nil {
-		return ""
-	}
-	matches := pattern.FindSubmatch(b)
-	if len(matches) == 0 {
-		return ""
-	}
-
-	quoted := string(matches[1])
-	unquoted := stringutil.Unquote(quoted)
-	if unquoted == quoted {
-		// NOTE Not a valid string
-		return ""
-	}
-	return unquoted
-}
-
-var gemSpecVariablePattern = regexp.MustCompile(`(?:^|\s)Gem::Specification\.new\s+do\s+\|([A-Za-z]\w*)\|`)
-
-// gemSpecVarBytes reads bytes representing a .gemspec file and attempts to get the variable name that represents the spec.
-func gemSpecVarBytes(b []byte) string {
-	matches := gemSpecVariablePattern.FindSubmatch(b)
-	if len(matches) != 0 {
-		return string(matches[1])
-	}
-
-	return ""
+// gemSpecFromPath creates a badge.fury.io based on the path to a .gemspec file.
+func gemSpecFromPath(path string) string {
+	base := filepath.Base(path)
+	name := strings.TrimSuffix(base, ".gemspec")
+	return fmt.Sprintf("[![Gem Version](https://badge.fury.io/rb/%[1]s.svg)](https://badge.fury.io/rb/%[1]s)", name)
 }
--- internal/badges/gemspec_test.go
+++ internal/badges/gemspec_test.go
@@ -1,116 +1,29 @@
 package badges
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/MakeNowJust/heredoc/v2"
-)
-
-func TestGemSpecNameBytes(t *testing.T) {
-	tests := []struct{
+func TestGemSpecFromPath(t *testing.T) {
+	tests := []struct {
 		name string
-		content string
+		path string
 		want string
 	}{
 		{
-			name: "spec.name is set",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |spec|
-				spec.name                   = 'foo'
-				spec.version                = '0.1.0'
-			`),
-			want: "foo",
+			name: "path is github-linguist.gemspec",
+			path: "github-linguist.gemspec",
+			want: "[![Gem Version](https://badge.fury.io/rb/github-linguist.svg)](https://badge.fury.io/rb/github-linguist)",
 		},
 		{
-			name: "double quotes",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |spec|
-				spec.name                   = "foo-bar"
-				spec.version                = '0.1.0'
-			`),
-			want: "foo-bar",
-		},
-		{
-			name: "double quotes",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |spec|
-				spec.name                   = "foo-bar"
-				spec.version                = '0.1.0'
-			`),
-			want: "foo-bar",
-		},
-		{
-			name: "spec variable is s",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |s|
-				s.name                   = "foo-bar"
-				s.version                = '0.1.0'
-			`),
-			want: "foo-bar",
-		},
-		{
-			name: "name is not set",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |s|
-				s.version                = '0.1.0'
-			`),
-			want: "",
+			name: "path is path/to/github-linguist.gemspec",
+			path: "path/to/github-linguist.gemspec",
+			want: "[![Gem Version](https://badge.fury.io/rb/github-linguist.svg)](https://badge.fury.io/rb/github-linguist)",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := gemSpecNameBytes([]byte(tt.content)); got != tt.want {
-				t.Fatalf(`gemSpecNameBytes() = %q, want %q`, got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGemSpecVarBytes(t *testing.T) {
-	tests := []struct{
-		name string
-		content string
-		want string
-	}{
-		{
-			name: "variable is called spec",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |spec|
-				spec.name                   = 'foo'
-				spec.version                = '0.1.0'
-			`),
-			want: "foo",
-		},
-		{
-			name: "spec variable is s",
-			content: heredoc.Doc(`
-				# frozen_string_literal: true
-
-				Gem::Specification.new do |s|
-				s.name                   = "foo-bar"
-				s.version                = '0.1.0'
-			`),
-			want: "foo-bar",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := gemSpecNameBytes([]byte(tt.content)); got != tt.want {
-				t.Fatalf(`gemSpecNameBytes() = %q, want %q`, got, tt.want)
+			if got := gemSpecFromPath(tt.path); got != tt.want {
+				t.Fatalf(`gemSpecFromPath() = %q, want %q`, got, tt.want)
 			}
 		})
 	}
